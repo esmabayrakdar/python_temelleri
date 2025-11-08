03299cd4041c986c12c58b9b6294f7681b57738b
Esma BAYRAKDAR <beesma.04@gmail.com>
2025-11-08 13:50:12 +0300

    Add files via upload

---
+++ 9. pythonda modüller/math.py
@@ -0,0 +1,29 @@
+#YÖNTEM 1
+# import math
+
+# value = dir(math)
+# value = help(math)
+# value = help(math.ceil)
+# value = math.sqrt(49)
+# value = math.factorial(5)
+# value = math.floor(5.9)
+# value = math.ceil(5.9)
+
+# import math as islem
+
+# value = islem.factorial(5)
+
+#YÖNTEM 2
+# from math import*
+
+def sqrt(x):
+    print('x: '+ str(x))
+
+
+from math import factorial, sqrt, ceil
+
+# value = factorial(5)
+value = sqrt(9)
+# value = ceil(9.8)
+
+print(value)
---
+++ 9. pythonda modüller/mod.py
@@ -0,0 +1,23 @@
+'''
+    modül hakkında bilgilendirme
+'''
+print('modül eklendi')
+number = 10
+
+numbers = [1,2,3]
+
+person = {
+    "name": "Esma",
+    "age": "21",
+    "city": "Bursa"
+}
+
+def func(x):
+    '''
+        fonksiyon hakkında bilgilendirme
+    '''
+    print(f'x: {x}')
+
+class Person:
+    def speak(self):
+        print('I am speaking')
